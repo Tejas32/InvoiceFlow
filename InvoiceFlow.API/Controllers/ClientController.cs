f32afcccb12026673e45888f07b478144cf27663
Tejas Mundhe <tdmundhe32@gmail.com>
2026-05-14 00:11:19 +0530

    feat: controllers created seperately

---
+++ InvoiceFlow.API/Controllers/ClientController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InvoiceFlow.API.Controllers
+{
+    public class ClientController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ InvoiceFlow.API/Controllers/InvoiceController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InvoiceFlow.API.Controllers
+{
+    public class InvoiceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
